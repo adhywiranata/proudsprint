465ac740089a5eabb9836657c22e207d12be6278
Adhy <adhywiranatap@gmail.com>
2017-02-16 17:35:28 +0700

    added Header shallow rendering

--- __tests__/react.test.jsx
+++ __tests__/react.test.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-// import Header from '../src/components/layouts/Header';
+import Header from '../src/components/layouts/Header';
 import Topbar from '../src/components/layouts/Topbar';
+import Navbar from '../src/components/layouts/Navbar';
 
 describe('<Topbar />', () => {
-  it('renders Topbar', () => {
+  it('renders itself', () => {
     const wrapper = shallow(<Topbar />);
-    expect(wrapper).to.have.length(1);
+    expect(wrapper).toHaveLength(1);
   });
 });
 
-// describe('<Header />', () => {
-//   it('renders Topbar', () => {
-//     const wrapper = shallow(<Header />);
-//     expect(wrapper.find(Topbar)).to.have.length(1);
-//   });
-// });
+describe('<Header />', () => {
+  it('renders Topbar as child', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Topbar)).toHaveLength(1);
+  });
+  it('renders Navbar as child', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(Navbar)).toHaveLength(1);
+  });
+  // TODO test Header passed props
+});
