8031800d702452b03418b4274c40157385efbf89
Adhy <adhywiranatap@gmail.com>
2017-02-22 08:59:29 +0700

    updated server index

--- server/index.js
+++ server/index.js
@@ -14,6 +14,6 @@ app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '../public', 'index.html'));
 });
 
-app.listen(port, function() {
-  console.log('Server is running on port ' + port);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
